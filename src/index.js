a51b7fb9c57083d14ff782cc29e48376d3f829fa
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-10-23 02:57:18 +0300

    Import installed packges & add navbar component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Navbar from './components/Navbar';
 import reportWebVitals from './reportWebVitals';
+import 'jquery';
+import 'popper.js/dist/umd/popper';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar />
     <App />
   </React.StrictMode>,
 );
