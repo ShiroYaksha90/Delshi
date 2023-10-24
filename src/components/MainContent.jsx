bad5045870da6b85231fe224cfcad6421ca2c359
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-10-24 23:59:11 +0300

    Add statinc data to render & click event

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
 function MainContent() {
+  const state = { pageTitle: 'Customers', customerCount: 5 };
+  const onRefeshClick = () => {
+    console.log('Refresh clicked');
+  };
   return (
     <div>
       <main>
-        <p>This is where most of my content will go...</p>
+        <h4 className="border-bottom m-1 p-1">
+          {state.pageTitle}
+          <span className="badge bg-secondary m-2">
+            {
+            state.customerCount
+}
+          </span>
+          <button className="btn btn-info" onClick={onRefeshClick} type="submit">Refresh</button>
+        </h4>
       </main>
     </div>
   );
